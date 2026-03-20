20881713327d0c979b1858d4a5e9efcc3768e74b
Man <man.ng87@googlemail.com>
2026-03-20 17:35:25 +0000

    base functions done

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 if (import.meta.env.DEV) {
+  import("./js/baseFunctions.js")
   import("./css/baseStyles.scss")
 }
 
 // console.log(import.meta.env, 'TEST HERE 1')
 
-function test () {
+function init () {
   let test = 'test'
   console.log(test, 'TEST HERE 2')
 }
 
-test()
+init()
